30036173d9d1bb1d2a6276ce49f72ddb24785ad4
Quentin Ramecourt <pro.quentin.ramecourt@gmail.com>
2022-07-28 10:15:26 +0200

    ajout des nouvelles questions

--- front/script.js
+++ front/script.js
@@ -1,3 +1,13 @@
+
 function showQuestion2() {
     document.getElementById('question2').style.visibility="visible"
+}
+function showQuestion3() {
+    document.getElementById('question3').style.visibility="visible"
+}
+function showQuestion4() {
+    document.getElementById('question4').style.visibility="visible"
+}
+function showQuestion5() {
+    document.getElementById('question5  ').style.visibility="visible"
 }
